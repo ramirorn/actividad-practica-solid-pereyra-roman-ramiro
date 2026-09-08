74d8bdede3732b6638903390a28fcd0ebac74778
Ramiro <ramiroroman306@gmail.com>
2026-09-08 12:06:49 -0300

    Actividad 2. OCP: Open/Closed Principle / Terminada

--- ejercicios/02-ocp.ts
+++ ejercicios/02-ocp.ts
@@ -1,18 +1,33 @@
-type PaymentType = "card" | "cash";
+interface PaymentMethod {
+  pagar(cantidad: number): void;
+}
 
-class PaymentProcessor {
-  pay(type: PaymentType, amount: number): void {
-    switch (type) {
-      case "card":
-        console.log(`Pagando $${amount} con tarjeta`);
-        break;
-      case "cash":
-        console.log(`Pagando $${amount} en efectivo`);
-        break;
-      default:
-        throw new Error("Medio de pago no soportado");
-    }
+class Tarjeta implements PaymentMethod {
+  pagar(cantidad: number): void {
+    console.log(`Pagó ${cantidad} por tarjeta`)
   }
 }
 
-new PaymentProcessor().pay("card", 100);
+class Efectivo implements PaymentMethod {
+  pagar(cantidad: number): void {
+    console.log(`Pagó ${cantidad} en efectivo`)
+  }
+}
+
+class Transferencia implements PaymentMethod {
+  pagar(cantidad: number): void {
+    console.log(`Pagó ${cantidad} por transferencia`)
+  }
+}
+
+class PaymentProcessor {
+  implementarPago(metodo: PaymentMethod, cantidad: number) {
+    metodo.pagar(cantidad);
+  }
+}
+
+const procesador = new PaymentProcessor();
+
+procesador.implementarPago(new Tarjeta(), 100)
+procesador.implementarPago(new Efectivo(), 100)
+procesador.implementarPago(new Transferencia(), 100)
