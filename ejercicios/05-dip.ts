c22f0a1c8a6bdc16f850c279af294e6a79473b88
Ramiro <ramiroroman306@gmail.com>
2026-09-09 12:15:34 -0300

    Actividad 5. DIP: Dependency Inversion Principle / Terminada

--- ejercicios/05-dip.ts
+++ ejercicios/05-dip.ts
@@ -1,16 +1,34 @@
-class EmailSender {
+interface Notifier {
+  send(to: string, message: string): void;
+}
+
+class EmailSender implements Notifier {
   send(to: string, message: string): void {
     console.log(`Correo para ${to}: ${message}`);
   }
 }
 
+
+class SmsSender implements Notifier {
+  send(to: string, message: string): void {
+    console.log(`SMS para ${to}: ${message}`)
+  }
+}
 class OrderService {
-  createOrder(customerEmail: string): void {
+  private notifier: Notifier;
+
+  constructor(notificador: Notifier) {
+    this.notifier = notificador;
+  }
+
+  createOrder(customerContact: string): void {
     console.log("Pedido creado");
 
-    const emailSender = new EmailSender();
-    emailSender.send(customerEmail, "Tu pedido fue creado");
+    this.notifier.send(customerContact, "Tu mensaje fue creado exitosamente")
   }
 }
 
-new OrderService().createOrder("ana@example.com");
+const emailNotifier = new EmailSender()
+
+const orderWithEmail = new OrderService(emailNotifier);
+orderWithEmail.createOrder("ramiroroman306@gmail.com")
